074bc046b49de8048d745f6d8f729ca6b4d857fa
Jullies <julliesnyash@gmail.com>
2021-02-05 21:38:47 +0300

    binary

--- trading_bot.py
+++ trading_bot.py
@@ -6,18 +6,32 @@ class startProgram:
         self.username = username
         self.password = password
         self.liveorDemo = liveorDemo
-        self.loginToAccount()
+        self.startingBot()
+    
+    def runningBot(self):
+        if self.liveorDemo == 'demo':
+            self.I_want_money.change_balance('PRACTICE')
+        elif self.liveorDemo == 'live':
+            self.I_want_money.change_balance('REAL')
+        else:
+            print('Oops i could not understand which sesion you choosed')
+            exit()
+        print('\nYour account balance is: %s %s'%(self.I_want_money.get_balance(), self.I_want_money.get_currency()))
+            
+    
+    def resetDemoBalance(self):
+        self.I_want_money.reset_practice_balance()
         
     
-    def loginToAccount(self):
+    def startingBot(self):
         print('Connecting to your account')
-        I_want_money=IQ_Option(self.username, self.password)
-        I_want_money.connect()#connect to iqoption
+        self.I_want_money=IQ_Option(self.username, self.password)
+        self.I_want_money.connect()#connect to iqoption
         error_password="""{"code":"invalid_credentials","message":"You entered the wrong credentials. Please check that the login/password is correct."}"""
         iqoption = IQ_Option("email", "password")
         check,reason=iqoption.connect()
         if check:
-            print("Start your robot")
+            print("Starting your robot")
             #if see this you can close network for test
             while True: 
                 if iqoption.check_connect()==False:#detect the websocket is close
@@ -32,7 +46,7 @@ class startProgram:
                             print("No Network")
                 else:
                     print('Connection was successfull')
-                    time.sleep(1)
+                    self.runningBot()
         else:
             
             if reason=="[Errno -2] Name or service not known":
@@ -42,4 +56,4 @@ class startProgram:
                 
                 
 if __name__ == '__main__':
-    startProgram("julliesnyash@gmail.com","@PSD95isNOTt" ,'demo')
+    startProgram("julliesnyash@gmail.com","@PSD95isNOTt" ,'demo') #Use demo/live
